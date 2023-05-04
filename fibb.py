894646a423f6f747305a199c065a9a476cbdd252
kirbisak <110771112+kirbisak@users.noreply.github.com>
2023-05-04 09:21:19 +0200

    Update fibb.py

--- fibb.py
+++ fibb.py
@@ -1,19 +1,18 @@
 def fibb(n):
-    if n <= 0:
-        return None
-    elif n == 1:
-        return 1
-    else:
-        a = 0
-        b = 1
-        for i in range(1, n + 1):
-            c = a + b
-            a = b
-            b = c
-        zlaty_rez = b / a
-        return zlaty_rez
+    a = 0
+    b = 1
+    c = 0
+    for i in range(n):
+        c = a + b
+        a = b
+        b = c
+        
+    return a
 
 
-n = int(input("Zadaj pocet clenov fibb postupnosti: "))
-if fibb(n) != None:
-    print(f"Hodnota zlateho rezu z {n} clenov, je priblizne: {fibb(n)}")
+n = 1
+while fibb(n+1) / fibb(n) != fibb(n+2) / fibb(n+1):
+    n += 1
+else:
+    print(n)
+    print(fibb(n+1) / fibb(n))
